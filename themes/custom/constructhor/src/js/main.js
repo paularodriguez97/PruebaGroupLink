5b65dbb4cfb14a13553ca1c5734ca902797367d4
prodriguez@seedem.co <prodriguez@seedem.co>
2022-02-23 23:03:19 -0500

    CUerpo

---
+++ themes/custom/constructhor/src/js/main.js
@@ -0,0 +1,9 @@
+// let modal = document.querySelector('.modal-notification');
+// let modal_close = document.querySelector('.modal-close ');
+
+// if(!!modal && !!modal_close){
+//     modal_close.addEventListener("click", () => {
+//         modal.classList.remove('show');
+//     });
+// }
+
